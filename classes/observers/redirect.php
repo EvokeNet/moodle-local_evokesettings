de2f3bd879008248ec48e1320b681e085a7147e2
Willian Mano <willian.mano@jove.com>
2023-03-31 13:01:32 -0300

    Add fr language and improve logout event observer

--- classes/observers/redirect.php
+++ classes/observers/redirect.php
@@ -16,7 +16,7 @@ use core\event\base as baseevent;
 
 class redirect {
     public static function observer(baseevent $event) {
-        global $USER, $CFG;
+        global $USER, $CFG, $DB;
 
         if (is_siteadmin()) {
             return;
@@ -25,25 +25,33 @@ class redirect {
         if ($event instanceof \core\event\course_viewed) {
             $data = $event->get_data();
 
-            if ($data['courseid'] > 1) {
-                return;
+            if ($data['courseid'] == 1) {
+                $courses = enrol_get_all_users_courses($USER->id);
+
+                if (empty($courses) || count($courses) > 1) {
+                    return;
+                }
+
+                $course = current($courses);
+
+                redirect(new \moodle_url('/course/view.php', ['id' => $course->id]));
             }
         }
 
         if ($event instanceof \core\event\user_loggedout) {
-            $encodedredirecturi = urlencode($CFG->wwwroot);
+            $user = $DB->get_record('user', ['id' => $event->userid]);
 
-            return redirect(new \moodle_url("https://auth.evokenet.org/auth/realms/evoke/protocol/openid-connect/logout?redirect_uri={$encodedredirecturi}"));
+            if ($user->auth == 'oauth2') {
+                $issuer = $DB->get_record_sql("SELECT * FROM {oauth2_issuer} WHERE baseurl like '%evokenet%'");
+
+                if (!$issuer) {
+                    return;
+                }
+
+                $encodedredirecturi = urlencode($CFG->wwwroot);
+
+                return redirect(new \moodle_url("{$issuer->baseurl}/protocol/openid-connect/logout?redirect_uri={$encodedredirecturi}"));
+            }
         }
-
-        $courses = enrol_get_all_users_courses($USER->id);
-
-        if (empty($courses) || count($courses) > 1) {
-            return;
-        }
-
-        $course = current($courses);
-
-        redirect(new \moodle_url('/course/view.php', ['id' => $course->id]));
     }
 }
---
+++ lang/fr/local_evokesettings.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Lang file.
+ *
+ * @package     local_evokegame
+ * @copyright   2021 World Bank Group <https://worldbank.org>
+ * @author      Willian Mano <willianmanoaraujo@gmail.com>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string[' pluginname '] = 'Paramètres Evoke';
+
+$string['coursesettings'] = 'Les paramètres du cours Evoke';
+$string['coursemenuitems'] = 'Éléments du menu du cours';
+$string['coursemenuitems_help'] = 'Des éléments de menu supplémentaires seront ajoutés ici. Entrez chaque élément de menu sur une nouvelle ligne avec le format: texte de menu et une URL de lien. Par exemple:
+<pre>
+Annonces|/course/
+Chat d\'équipe|/mod/chat/view.php?id =2
+Forum du cours|/mod/forum/view.php?id =3
+</pre>';
+$string['pages'] = 'Des pages Evoke';
+$string['coursesettings_success'] = 'Paramètres du cours enregistrés avec succès.';
+
+$string['actions'] = 'Actions';
+$string['page_create'] = 'Créer une page';
+$string['page_title'] = 'Titre de la page';
+$string['page_slug'] = 'Slug de la page';
+$string['page_showonmenu'] = 'Afficher dans le menu?';
+$string['page_content'] = 'Contenu de la pag';
+
+$string['validation:minlen'] = 'Ce champ doit contenir au moins {$a} caractères.';
+
+$string['confirm_yes'] = 'Oui, je le veux!';
+$string['confirm_no'] = 'Annuler';
+
+$string['createitem_success'] = 'Élément créé avec succès';
+$string['updateitem_success'] = 'Article mis à jour avec succès';
+$string['deleteitem_success'] = 'Élément supprimé avec succès';
+
+$string['deleteitem_confirm_title'] = 'Êtes-vous sûr?';
+$string['deleteitem_confirm_msg'] = 'Une fois supprimé, cet élément ne peut pas être récupéré!';
